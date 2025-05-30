4baac58bc4a523f4fd3c56b8544ef650dec988bf
Amaobi Victor <aobikobe@gmail.com>
2025-05-30 11:25:05 +0100

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/HIo4jz0Bt4

---
+++ apps/task_management_system/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'task_management_system',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/task_management_system',
+};
